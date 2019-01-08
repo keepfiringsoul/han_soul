8e28c88bf11a31724460f60db58e8795c4afe718
keepfiringsoul <keepfiringsoul@gmail.com>
2019-01-09 08:56:16 +0900

    c2

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -15,3 +15,50 @@ function gestureStart() {
     }
   }
 }
+
+
+var scene, screen, closeBt, t_box, mouseOption;
+var item_Num = 27;
+var pf = [];
+var rd = [];
+var rd_x = [];
+var rd_y = [];
+var rd_z = [];
+
+
+var init = function() {
+
+for(var i=0;i<item_Num; i++){
+   
+    var d = document.createElement('div');
+    d.id='b_'+i;
+    d.className = 'block';
+      d.style['width'] = block.width+'px';
+      d.style['height'] = block.height+'px';
+      d.style['background-image']='url(./images/'+i+'.png)';
+      d.style['-webkit-mask-image']='url(./images/mask.png)';
+      scene.appendChild(d);
+  
+    pf.push(d);
+    d.onclick= function() {
+      contentsFill(this);
+      screen.style['display'] = 'block';
+      closeBt.style['display'] = 'block';
+      setTimeout(function(){
+        screen.style['opacity'] = '0.8';
+        closeBt.style['opacity'] = '1';
+      },50);    
+      };
+    
+    d.onmouseover = function(e){ 
+      //var s = 'translate3d('+rd_x[i]+'px,'+rd_y[i]+'px,'+rd_z[i]+'px) rotateY(360deg)';
+      var s = 'translate3d('+this.rd_x+'px,'+ this.rd_y+'px,'+ this.rd_z +'px) rotateY(360deg)';
+      this.style['-webkit-transform'] = s;
+      console.log(this.id);
+      console.log(s);
+    };
+    d.onmouseout = function(e){ 
+      this.style['-webkit-transform'] = 'translate3d('+this.rd_x+'px,'+ this.rd_y+'px,'+ this.rd_z +'px) rotateY(720deg)';
+    };  
+
+  }
