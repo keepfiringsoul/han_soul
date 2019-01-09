acb85ed680c6f2bd1703013595b780d817fcf1f6
keepfiringsoul <keepfiringsoul@gmail.com>
2019-01-09 15:42:59 +0900

    c4

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -77,3 +77,63 @@ window.onload = function() {
   init();
 
 };
+
+
+
+//current position
+var pos = 0;
+//number of slides
+var totalSlides = $('#slider-wrap ul li').length;
+//get the slide width
+var sliderWidth = $('#slider-wrap').width();
+
+
+$(document).ready(function(){
+    
+    /*****************
+     BUILD THE SLIDER
+    *****************/
+    //set width to be 'x' times the number of slides
+    $('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
+    
+    //next slide    
+    $('#arrow_right').click(function(){
+        slideRight();
+    });
+    
+    //previous slide
+    $('#arrow_left').click(function(){
+        slideLeft();
+    });
+    
+});//DOCUMENT READY
+    
+
+
+/***********
+ SLIDE LEFT
+************/
+function slideLeft(){
+    pos--;
+    if(pos==-1){ pos = totalSlides-1; }
+    $('#slider-wrap ul#slider').css('left', -(sliderWidth*pos));    
+    
+    //*> optional
+    countSlides();
+    pagination();
+}
+
+
+/************
+ SLIDE RIGHT
+*************/
+function slideRight(){
+    pos++;
+    if(pos==totalSlides){ pos = 0; }
+    $('#slider-wrap ul#slider').css('left', -(sliderWidth*pos)); 
+    
+    //*> optional 
+    countSlides();
+    pagination();
+}
+
